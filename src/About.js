fa01934fccb0f6804f7d28c823645fea7ba84b62
codewithbalaji <balaji01975@gmail.com>
2023-08-06 09:56:28 +0530

    first commit

---
+++ src/About.js
@@ -0,0 +1,13 @@
+const About = () => {
+  return (
+    <main className="About">
+      <h2>About</h2>
+      <p style={{ marginTop: '1rem' }}>
+        Hi, I am balaji. Im learning react and created this social media app ,
+        My mentor is Balachandar
+      </p>
+    </main>
+  );
+};
+
+export default About;
